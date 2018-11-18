af9098e30a015c9a111fa1c6c46a4270eff7804a
Dennis Mitchell <CPUsyntax@gmail.com>
2018-11-18 12:28:43 -0500

    Complete

---
+++ javascript/Objects/exercise7/script.js
@@ -0,0 +1,30 @@
+{
+    username: "Dennis"
+    password: "Fart"    
+}
+{
+    username: "Eddy"
+    password: "smells"    
+}
+
+var database = [
+    {
+        username: "Dennis",
+        password: "Fart"
+    }
+];
+
+var newsfeed = [
+    {
+        username: "Bobby",
+        timeline: "So tired"
+    },
+    {
+        username: "Edith",
+        timeline: "Javascript is so cool!"
+    },
+    {
+        username: "Mitch",
+        timeline: "lmao"
+    }
+];
