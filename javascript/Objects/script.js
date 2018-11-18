271ff7cc87ba32a988ebd5178616edd83848f59d
Dennis Mitchell <CPUsyntax@gmail.com>
2018-11-17 22:05:22 -0500

    Objects

---
+++ javascript/Objects/script.js
@@ -0,0 +1,44 @@
+// Object
+var user = {
+    name: "John",
+    age: 34,
+    hobby: "Soccer",
+    isMarried: false,
+    shout: function() {
+        console.log("AHHHHHHHH!");
+    },
+};
+
+console.log(user);
+
+user.favoriteFood = "Spinach";
+
+console.log(user);
+
+user.Spells = ["Burning Hands", "Charm Person", "Feather Fall", "Magic Missile", "Animate Dead"];
+
+console.log(user);
+
+var list = [
+    {
+        username: "Andy",
+        password: "secret",
+    },
+    {
+        username: "jess",
+        password: "123",
+    }
+]
+
+console.log(user.Spells);
+console.log(user.Spells[1]);
+console.log(list[0].password);
+
+
+var emptyObj = {};
+var nullObj = null;
+
+
+
+
+
